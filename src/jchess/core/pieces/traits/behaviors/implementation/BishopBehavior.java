a9187b627d8913e569c99bbe4924f4c47516fab1
KorriZokta <frederic.becker4@gmail.com>
2015-04-14 15:01:58 +0200

    Just to push

--- src/jchess/core/pieces/traits/behaviors/implementation/BishopBehavior.java
+++ src/jchess/core/pieces/traits/behaviors/implementation/BishopBehavior.java
@@ -44,7 +44,7 @@ public class BishopBehavior extends LongRangePieceBehavior
         list.addAll(getMovesForDirection(DIRECTION_LEFT,   DIRECTION_BOTTOM)); //left-down
         list.addAll(getMovesForDirection(DIRECTION_RIGHT,  DIRECTION_UP)); //right-up
         list.addAll(getMovesForDirection(DIRECTION_RIGHT,  DIRECTION_BOTTOM)); //right-down
-        
+        //ici
         return list;
     }
     
